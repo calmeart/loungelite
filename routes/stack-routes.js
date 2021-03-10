0d55126aff1e9e6cad35792098fb33cdd70d241b
batuhan öztürk <batuhandoruk91@gmail.com>
2021-03-10 20:22:02 +0200

    structure stack endpoints into routes folder

--- routes/stack-routes.js
+++ routes/stack-routes.js
@@ -1,19 +1,25 @@
 const express = require('express');
-const router = express.Router();
-const User = require('../models/user-model');
+const router = express.Router({mergeParams: true});
 const Post = require('../models/post-model');
 const Stack = require('../models/stack-model');
 const {formatDate} = require('../format-date.js');
-const stackRoutes = require('./stack-routes');
 
-router.use('/:userid/stacks', stackRoutes);
+router.route("/")
+  .post(async (req, res) => {
+    const tempStack = new Stack({
+      stackName: req.body.newStack,
+      userId: req.user._id
+    });
+    await tempStack.save();
+    res.redirect('/users/' + req.user._id);
+  });
 
-router.route("/:userid")
+router.route("/:stackname")
   .get(async (req, res) => {
     if (req.user) {
       const timeAgoArray = [];
-      const foundPosts = await Post.find({userIdNumber: req.user._id}).sort({date: "desc"});
-      const foundStacks = await Stack.find({userId: req.user._id});
+      const foundPosts = await Post.find({userIdNumber: req.params.userid, stack: req.params.stackname}).sort({date: "desc"});
+      const foundStacks = await Stack.find({userId: req.params.userid});
       foundPosts.forEach(item => {
         timeAgoArray.push(formatDate(item.date));
       });
@@ -27,6 +33,6 @@ router.route("/:userid")
       req.flash("error", "You need to login to view this page");
       res.redirect("/");
     }
-  });
+  })
 
 module.exports = router;
